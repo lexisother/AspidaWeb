9b93c14262b9ee2b78e58916d18604c0484ed33e
Alyxia Sother <lexisoth2005@gmail.com>
2021-07-25 01:05:34 +0200

    [Home] Added quote to header

--- src/Home.tsx
+++ src/Home.tsx
@@ -5,7 +5,10 @@ export default function HomePage(): JSX.Element {
     return (
         <Page>
             <div className="header">
-                <h1>Home</h1>
+                <h1>Aspida</h1>
+                <p className="quote">
+                    <i>"Kinda like MTF but more cool stuff" ~ Nico</i>
+                </p>
             </div>
             <div className="content">
                 <p>test</p>
