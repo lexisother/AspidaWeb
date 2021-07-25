81569e134cf5ed63ce1a6efffdab80b179b84978
Alyxia Sother <lexisoth2005@gmail.com>
2021-07-26 00:02:49 +0200

    [Scripts] Added user fetching script

---
+++ scripts/fetchMembers.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+API_BASE="https://discord.com/api/v9"
+GUILD_ID="867710362341081118"
+
+data=$(curl -H "Authorization: Bot $BOT_TOKEN" "$API_BASE/guilds/$GUILD_ID/members?limit=50")
+jq 'map(select(.user.bot != true)) | [.[] | {name: .user.username, nick: .nick, avatar: .user.avatar}]' <<< "$data" > data.json
