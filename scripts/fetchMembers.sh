78edc6ea64f6fed92f5a8256a0251e61e0cecfbb
Alyxia Sother <lexisoth2005@gmail.com>
2021-07-26 14:07:28 +0200

    [Scripts] Also fetch user ID

--- scripts/fetchMembers.sh
+++ scripts/fetchMembers.sh
@@ -4,4 +4,4 @@ API_BASE="https://discord.com/api/v9"
 GUILD_ID="867710362341081118"
 
 data=$(curl -H "Authorization: Bot $BOT_TOKEN" "$API_BASE/guilds/$GUILD_ID/members?limit=50")
-jq 'map(select(.user.bot != true)) | [.[] | {name: .user.username, nick: .nick, avatar: .user.avatar}]' <<< "$data" > data.json
+jq 'map(select(.user.bot != true)) | [.[] | {id: .user.id, name: .user.username, nick: .nick, avatar: .user.avatar}]' <<< "$data" > data.json
