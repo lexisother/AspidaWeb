b0a23d58ec81cdafdbf57d16eee383cd4ac40781
Alyxia Sother <lexisoth2005@gmail.com>
2021-07-26 18:05:53 +0200

    [Scripts] Fetch all roles

--- scripts/fetchMembers.sh
+++ scripts/fetchMembers.sh
@@ -3,5 +3,12 @@
 API_BASE="https://discord.com/api/v9"
 GUILD_ID="867710362341081118"
 
-data=$(curl -H "Authorization: Bot $BOT_TOKEN" "$API_BASE/guilds/$GUILD_ID/members?limit=50")
-jq 'map(select(.user.bot != true)) | [.[] | {id: .user.id, name: .user.username, nick: .nick, avatar: .user.avatar}]' <<< "$data" > data.json
+if [ ! -d 'data' ]; then
+  mkdir data
+fi
+
+memberdata=$(curl -H "Authorization: Bot $BOT_TOKEN" "$API_BASE/guilds/$GUILD_ID/members?limit=50")
+jq 'map(select(.user.bot != true)) | [.[] | {id: .user.id, name: .user.username, nick: .nick, avatar: .user.avatar, roles: .roles}]' <<< "$memberdata" > data/members.json
+
+roledata=$(curl -H "Authorization: Bot $BOT_TOKEN" "$API_BASE/guilds/$GUILD_ID/roles")
+jq 'map(select(.managed != true)) | [.[] | {name: .name, id: .id, color: .color}]' <<< $roledata > data/roles.json
